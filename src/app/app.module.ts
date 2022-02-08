40206b2e9851a5796658c173c459498e02b012fa
lopesleandro.it <lopesleandro.it@outlook.com>
2022-02-08 16:03:05 -0300

    criado o componente para listar os repositórios e alterado o template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonalInfoComponent
+    PersonalInfoComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
